62ac7b0eb39a05c7596acea9486c6abcb80189ea
Mauricio Reyna <mauriciojreyna@gmail.com>
2018-11-14 21:09:28 -0500

    add the error

---
+++ Lab24/Models/Items.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Lab24.Models
+{
+    public class Items
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public float Price { get; set; }
+    }
+
+    public class DBItemContex : DbContext
+    {
+        public DbSet<Items> Item { get; set; }
+    }
+}
