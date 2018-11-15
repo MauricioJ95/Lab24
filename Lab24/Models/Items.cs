3d7c5928abd3bf737966086a281eb0f4ae570928
Mauricio Reyna <mauriciojreyna@gmail.com>
2018-11-14 21:44:34 -0500

    Update gitignore

--- Lab24/Models/Items.cs
+++ Lab24/Models/Items.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
 
 namespace Lab24.Models
 {
     public class Items
     {
+        [Key]
         public int ID { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
